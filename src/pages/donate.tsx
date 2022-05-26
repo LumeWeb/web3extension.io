bc06f3713f180b792696a7780029d88cdfb56e77
Derrick Hammer <derrick@derrickhammer.com>
2022-05-26 05:01:10 -0400

    *typos and rewording

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -41,7 +41,7 @@ const DonatePage = () => {
             </div>
             <div className="content-body">
                 <h3>Why we ask for your donations</h3>
-                <p>At this time, Lume Web is a one-person show and frankly... a spec of dust on the internet, but with large ideas and a unique vision. We are relying on the community, and crypto whales to support this initiative to build the technology the internet needs to be owned by the users its serves.</p>
+                <p>At this time, Lume Web is a one-person show and frankly... a speck of dust on the internet, but with large ideas and a unique vision. We are relying on the community, and crypto whales to support this initiative to build the technology the internet needs to be owned by the users its serves.</p>
                 <p>The current internet is controlled by a few for them to make money with you as the product, in exchange for all the "free" stuff you get in return. If you think on it, you will find a pattern here, and it turns us to becoming batteries in the matrix.</p>
             </div>
         </section>
@@ -49,7 +49,7 @@ const DonatePage = () => {
 
         <section className="text">
             <div className="text-body">
-                <h3>Payment possibilities</h3>
+                <h3>$$ How to support $$</h3>
                 <p>We aren't picky 😉. If there is a way you want to pay us, we are happy to take it. The project is funded through Hammer Technologies LLC, in the USA, and so crypto, FOSS platforms, and traditional payment methods are all accepted! Just click donate at the top to see your options, and to contact us if needed.</p>
             </div>
         </section>
@@ -60,7 +60,7 @@ const DonatePage = () => {
                 <h3>The way we are different</h3>
                 <p>First understand the .com boom/bubble/bust in the 1990's to 2000's was a gold rush for VC's to get rich or die trying. Outside VC's... everyone seemed to be smoking something and was looking to get rich too!</p>
                 <p>We have entered that era again, but this time with crypto, web3 and the "metaverse". Crypto is the money-arm of the web, web3 is to be the next internet, and at this time the "metaverse" is PR-filled hype and per the wolf of wallstreet.. <blockquote>It's a whazy. It's a woozie. It's fairy dust.</blockquote> If you want the metaverse, look at MMORPG, VR or just... real life 😝</p>
-           <p>The Lume Web project has no VC equity and will never give out equity in any form to investment companies since their motive is to make a profit usually by being a walled guarden and centralizing. VC's interests broadly go against the interest of a user-owned internet.</p>
+           <p>The Lume Web project has no VC equity and will never give out equity in any form to investment companies since their motive is to make a profit usually by being a walled garden and centralizing. VC's interests broadly go against the interest of a user-owned internet.</p>
             </div>
             <div className="content-img">
                 <StaticImage src={"../images/image-development-race.png"} alt="Roadmap image"/>
